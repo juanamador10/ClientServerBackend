eb58a098107cee52a9d26ddd7018b892ec711de1
Juan Amador <juan.amador.is@unipolidgo.edu.mx>
2021-08-01 03:12:37 -0500

    Second Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { userInfo } from 'os';
+import { UserEntity } from './user.entity';
+import { UsersService } from './users.service';
+
 
 @Controller('users')
 export class UsersController {
 
+    constructor (private service:UsersService){
+
+    }
+
 @Get()
 getAllUsers(){
-    return "List of Users...";
+    return this.service.getAllUsers();
 }
+
+@Get(":id")
+getUser(@Param() params){
+    return this.service.getUser(params.id);
+}
+
+@Post()
+addUser(@Body() user: UserEntity){
+    return this.service.createUser(user);
+}
+
+@Put()
+updateUser(@Body() user:UserEntity){
+    this.service.updateUser(user);
+}
+
+@Delete(":id")
+deleteUser(@Param() params){
+    this.service.deleteUser(params.id);
+}
+
+
+
+//@Patch(atributo:string){
+//    return "Update Attribute";
+//}
+
 
 }
